9bea34b0fc10c2a19721babb0de77ba691dcd740
Andrés Alejandro Plazas Malagón <plazas@Andress-MacBook-Pro.local>
2019-09-05 10:35:01 -0500

    First few comments/questions by AP

--- tools/corr.py
+++ tools/corr.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 
 from __future__ import print_function
 
@@ -7,39 +7,38 @@ try :
 except :
     import pyfits as pf
 
-    
 import numpy as np
 import sys
 
+#  The same routine could be written using (numpy) MaskedArrays.
+#  They do not seem that efficient, and we'll use a floating point mask
+#  to compute its Fourier transform
 
-
-# the same routine could be written using (numpy) MaskedArrays.
-# They do not seem that efficient, and we'll use a floating point mask
-# to compute its Fourier transform
 def find_mask(im, nsig, w=None) :
-    if w is None : w = np.ones(im.shape)
+    if w is None:
+        w = np.ones(im.shape)
     #  mu is used for sake of numerical precision in the sigma
-    # computation, and is needed (at full precision) to identify outliers. 
+    # computation, and is needed (at full precision) to identify outliers.
     count = w.sum()
     # different from (w*im).mean()
     mu = (w*im).sum()/count
-    # same comment for the variance. 
+    # same comment for the variance.
     sigma = np.sqrt((((im-mu)*w)**2).sum()/count)
-    for iter in range(3) :
-        outliers = np.where(np.abs((im-mu)*w)>nsig*sigma)
+    for iter in range(3):
+        outliers = np.where(np.abs((im-mu)*w) > nsig*sigma)
         w[outliers] = 0
         #  does not work :
         # count = im.size-ouliers[0].size
         count = w.sum()
         mu = (w*im).sum()/count
         newsig = np.sqrt((((im-mu)*w)**2).sum()/count)
-        if (np.abs(sigma-newsig)<0.02*sigma) :
+        if (np.abs(sigma-newsig) < 0.02*sigma):
             sigma = newsig
             break
         sigma = newsig
     return w
 
-def write_fits(filename, d) :
+def write_fits(filename, d):
     l = pf.HDUList()
     l.append(pf.ImageHDU(data=d))
     l.writeto(filename, clobber=True)
@@ -53,9 +52,9 @@ def fit_back(im, stepx, stepy=None) :
     """
     if stepy is None : stepy = stepx
     nx = im.shape[0]/stepx
-    if nx*stepx<im.shape[0] : nx += 1
+    if nx*stepx<im.shape[0]: nx += 1
     ny = im.shape[1]/stepy
-    if ny*stepy<im.shape[1] : ny += 1
+    if ny*stepy<im.shape[1]: ny += 1
     medians = np.ndarray((nx,ny))
     xpos = np.ndarray(nx)
     ypos = np.ndarray(ny)
@@ -89,62 +88,69 @@ def fit_back(im, stepx, stepy=None) :
 # This class is mostly justified by writing the "cov" function with
 # only 2 arguments.
 class cov_fft :
-    def __init__(self, diff,w, parameters) :
+    def __init__(self, diff, w, parameters):
         """
         This class computed (via FFT), the nearby pixel correlation function.
-        The range is controlled by "parameters", as well as 
-        the actual FFT shape.
+        The range is controlled by "parameters", as well as
+        the actual FFT shape.   # Assumes that w consists of 1's and 0's ? AP
         """
         self.parameters = parameters
         maxrange = parameters.maxrange
-        
+
         # check that the zero padding implied by "fft_shape"
         # is large enough for the required correlation range
-        fft_shape = parameters.fft_shape
-        assert(fft_shape[0]>diff.shape[0]+maxrange+1)
-        assert(fft_shape[1]>diff.shape[1]+maxrange+1)
+        fft_shape = parameters.fft_shape #  What is typical value for parameters.fft_shape? AP
+        assert(fft_shape[0] > diff.shape[0]+maxrange+1)
+        assert(fft_shape[1] > diff.shape[1]+maxrange+1)
         tim = np.fft.rfft2(diff*w, parameters.fft_shape)
         tmask = np.fft.rfft2(w, parameters.fft_shape)
-        # sum of  "squares"
+        # sum of  "squares" (What does this mean? Is there a reference I can consult? AP)
         self.pcov = np.fft.irfft2(tim*tim.conjugate())
-        # sum of values (depends on the offets indeed)
+        # sum of values (depends on the offets indeed) (What does this mean? AP)
         self.pmean= np.fft.irfft2(tim*tmask.conjugate())
-        # number of w!=0 pixels. 
+        # number of w!=0 pixels.
         self.pcount= np.fft.irfft2(tmask*tmask.conjugate())
-    
+
     def cov(self, dx,dy) :
         """
         covariance for dx,dy averaged with dx,-dy if both non zero.
         """
         # compensate rounding errors
-        npix1 = int(round(self.pcount[dy,dx]))
-        cov1 = self.pcov[dy,dx]/npix1-self.pmean[dy,dx]*self.pmean[-dy,-dx]/(npix1*npix1)
-        if (dx == 0 or dy == 0): return cov1, npix1
-        npix2 = int(round(self.pcount[-dy,dx]))
-        cov2 = self.pcov[-dy,dx]/npix2-self.pmean[-dy,dx]*self.pmean[dy,-dx]/(npix2*npix2)
+        # Can you explain this a bit more? AP
+        # How does this compensate for rounding errors? AP
+        npix1 = int(round(self.pcount[dy, dx]))
+        cov1 = self.pcov[dy, dx]/npix1-self.pmean[dy, dx]*self.pmean[-dy, -dx]/(npix1*npix1)
+        if (dx == 0 or dy == 0):
+            return cov1, npix1
+        npix2 = int(round(self.pcount[-dy, dx]))
+        cov2 = self.pcov[-dy, dx]/npix2-self.pmean[-dy, dx]*self.pmean[dy, -dx]/(npix2*npix2)
         return 0.5*(cov1+cov2), npix1+npix2
 
-    def report_cov_fft(self) :
+    def report_cov_fft(self):
         maxrange = self.parameters.maxrange
-        l = []
-        # (dy,dx) = (0,0) has to be first 
-        for dy in range(maxrange+1) :
-            for dx in range (0,maxrange+1) :
-                cov,npix = self.cov(dx,dy)
-                if (dx==0 and dy == 0) : var = cov
-                l.append((dx,dy,var,cov,npix))
+        tupleVec = []
+        # (dy,dx) = (0,0) has to be first
+        for dy in range(maxrange+1):
+            for dx in range(0, maxrange+1):  #  Why (0, maxRange+1) instead of just (maxRange+1)? AP
+                cov, npix = self.cov(dx, dy)
+                if (dx == 0 and dy == 0):
+                    var = cov
+                tupleVec.append((dx, dy, var, cov, npix))
 
-        return l
+        return tupleVec
 
-def compute_cov_fft(diff, w, parameters) :
+
+def compute_cov_fft(diff, w, parameters):
+    #  Use the class defined above
     c = cov_fft(diff, w, parameters)
     return c.report_cov_fft()
+
 
 def cov_direct_value(diff, w, dx,dy):
     """
     Computes in direct space the covariance of image diff at lag (i,j)
     for the pixels in w. Assumes that w contains 0's and 1's.
-    returns the covariance and the number of pixel pairs used to evaluate it.
+    Returns the covariance and the number of pixel pairs used to evaluate it.
     """
     (ncols,nrows) = diff.shape
     # switching both signs does not change anything:
@@ -164,16 +170,16 @@ def cov_direct_value(diff, w, dx,dy):
         w2 = w[-dy:, :nrows-dx]
     # use the same mask for all 3 calculations
     w_all = w1*w2
-    # do not use mean() because w=0 pixels would then count. 
+    # do not use mean() because w=0 pixels would then count.
     npix = w_all.sum()
     im1_times_w = im1*w_all
     s1 = im1_times_w.sum()/npix
     s2 = (im2*w_all).sum()/npix
     p = (im1_times_w*im2).sum()/npix
     cov = p-s1*s2
-    return cov,npix
-    
-    
+    return cov, npix
+
+
 
 
 def compute_cov_direct(diff, w, parameters) :
@@ -184,7 +190,7 @@ def compute_cov_direct(diff, w, parameters) :
     maxrange = parameters.maxrange
     l=[]
     var = 0
-    # (dy,dx) = (0,0) has to be first 
+    # (dy,dx) = (0,0) has to be first
     for dy in range(maxrange+1):
         for dx in range (0,maxrange+1) :
             if (dx*dy>0):
@@ -200,15 +206,15 @@ def compute_cov_direct(diff, w, parameters) :
 
 
 # assumes mask only contains 0's and 1's
-def masked_mean(im, mask) :
+def masked_mean(im, mask):
     return (im*mask).sum()/mask.sum()
 
 
-def fft_size(s) :
+def fft_size(s):
     # maybe there exists something more clever....
     x = int(np.log(s)/np.log(2.))
     return int(2**(x+1))
-    
+
 
 
 class ComputeCov :
@@ -216,7 +222,7 @@ class ComputeCov :
         self.im1 = im1
         self.im2 = im2
         self.params = params
-    
+
     def compute_cov(self):
         if (im1.check_images(im2) == False) :
             return []
@@ -231,7 +237,7 @@ class ComputeCov :
             other_values = ()
             other_tags = ()
         extensions = self.im1.segment_ids()
-        
+
         for ext in extensions :
             ext1 = self.im1.get_segment(ext)
             sim1_full = self.im1.prepare_segment(ext1)
@@ -249,7 +255,7 @@ class ComputeCov :
             w2 = find_mask(sim2, self.params.nsig1)
             # check is there are masks provided by the sensor itself (e.g. vignetting)
             ws = self.im1.sensor_mask(ext)
-            if ws is not None : 
+            if ws is not None :
                 ws = self.im1.clip_frame(ws)
                 w1 *= (1-ws)
                 w2 *= (1-ws)
@@ -265,7 +271,7 @@ class ComputeCov :
             if abs(fact-1)>0.1 :
                 print("%s and %s have too different averages in ext %d ! .... ignoring them"%(self.im1.filename, self.im2.filename, ext))
                 continue
-            # Compensate the flux difference, so that if the images are 
+            # Compensate the flux difference, so that if the images are
             # proportional to each other, any spatial structure vanishes.
             diff = (sim1*mu2-sim2*mu1)/(0.5*(mu1+mu2))
             w12 = w1*w2
@@ -283,8 +289,8 @@ class ComputeCov :
         tags = ['mu1','mu2','i','j','var','cov','npix','ext','s1','sp1','s2','sp2']+other_tags
         return tuple_rows, tags
 
-    
-    
+
+
 import pickle
 import scipy.interpolate as interp
 
@@ -313,49 +319,49 @@ class parameters :
 
 import argparse
 
-if __name__ == "__main__" :   
+if __name__ == "__main__" :
     params = parameters()
 
     # should provide a way to alter that from the command line
     help_fh_tags = '\n'.join(['         %s : %s'%(key,value) for key,value in file_handlers.iteritems()])
-    
+
     usage=" to compute covariances of differences of pairs of flats"
     parser = argparse.ArgumentParser(usage=usage)
     parser.add_argument("pairs_file", help= " list of pairs (2 filenames per line)")
-    
+
     parser.add_argument( "-t", "--tuple_name",
-                       dest = "tuple_name", 
+                       dest = "tuple_name",
                        type = str,
-                       help = "output tuple name (default : %(default)s)", 
+                       help = "output tuple name (default : %(default)s)",
                        default = "tuple.list"  )
     parser.add_argument( "-m", "--maxrange",
                        type=int,
-                       dest = "maxrange", 
-                       help = "how far we evaluate correlations", 
+                       dest = "maxrange",
+                       help = "how far we evaluate correlations",
                        default = 28  )
     parser.add_argument( "-j", "--jmax",
-                       dest = "jmax", 
+                       dest = "jmax",
                        type = int,
-                       help = "max j value considered in each extension", 
+                       help = "max j value considered in each extension",
                        default = "1990"  )
-    parser.add_argument( "-d", "--deferred-charge", 
+    parser.add_argument( "-d", "--deferred-charge",
                        action="store_true",  # default is False
-                       dest = "correct_deferred_charge", 
+                       dest = "correct_deferred_charge",
                        help = "correct for deferred charge (using ./cte.pkl)")
-    parser.add_argument( "-n", "--nonlin-correction", 
+    parser.add_argument( "-n", "--nonlin-correction",
                        action="store_true",  # default is False
-                       dest = "correct_nonlinearity", 
+                       dest = "correct_nonlinearity",
                        help = "correct non linearity (using ./nonlin.pkl)")
-    parser.add_argument( "-s", "--subtract_sky", 
+    parser.add_argument( "-s", "--subtract_sky",
                          action="store_true",  # default is False
-                         dest = "subtract_sky", 
+                         dest = "subtract_sky",
                          help = "subtract a smooth background from the difference")
-    parser.add_argument( "-f", "--file-handler", 
+    parser.add_argument( "-f", "--file-handler",
                          dest = "file_handler_tag",
                          help = help_fh_tags)
 
     options = parser.parse_args()
-#    if (len(args) == 0) : 
+#    if (len(args) == 0) :
 #        parser.print_help()
 #        sys.exit(1)
 
@@ -367,7 +373,7 @@ if __name__ == "__main__" :
     except KeyError:
         print('valid values for -f :\n%s',help_fh_tags)
         sys.exit(0)
-    
+
     params.correct_deferred_charge = options.correct_deferred_charge
     if options.correct_deferred_charge:
         try :
@@ -375,7 +381,7 @@ if __name__ == "__main__" :
         except IOError:
             print('cannot find ./cte.pkl for deferred charge correction')
             raise
-        print('INFO: loading deferred charge correction') 
+        print('INFO: loading deferred charge correction')
         params.dc_corr = pickle.load(f)
         f.close()
 
@@ -388,18 +394,18 @@ if __name__ == "__main__" :
         except IOError:
             print('cannot find ./nonlin.pkl for deferred charge correction')
             raise
-        print('INFO: loading nonlinearity correction') 
+        print('INFO: loading nonlinearity correction')
         params.nonlin_corr = pickle.load(f)
         f.close()
 
-    
-    
+
+
     #tuple = open(options.tuple_name,"w")
     #tuple.write('@OPTIONS %s\n'%' '.join(sys.argv))
     print('line commmand options : %s\n'%' '.join(sys.argv))
     np.seterr(invalid='raise')
     tuple_records = []
-    
+
     f = open(options.pairs_file)
     for i,l in enumerate(f.readlines()) :
         try :
@@ -428,4 +434,3 @@ if __name__ == "__main__" :
     nt = np.rec.fromrecords(tuple_records, names=alltags)
     print('writing tuple %s to disk'%options.tuple_name)
     np.save(options.tuple_name, nt)
-
