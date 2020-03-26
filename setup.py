11bd58ff4215e247b98ad04ce988b3e61e93f818
Pierre Astier <pierre.astier@in2p3.fr>
2020-03-26 13:17:04 +0100

    added a simple setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup, Extension
+import os
+import glob
+#from lib import __version__
+
+setup(name='croaks',
+      version='0.2',
+      description='Tools to analyze CCD flats in order to assemble photon transfer and covariance curves',
+      author='P. Astier',
+      author_email='pierre.astier@in2p3.fr',
+
+      packages=['bfptc'],
+      package_dir = {'bfptc': 'py'},
+      scripts=glob.glob('tools/*.py')
+)
