dc9f8cdd46080396d2a934beb01c6f50673fbe76
Pierre Astier <pierre.astier@in2p3.fr>
2022-07-08 16:51:37 +0200

    added cross-covariance computation capability

--- tools/corr.py
+++ tools/corr.py
@@ -106,8 +106,7 @@ def cov_direct_value(diff, w, dx,dy):
     p = (im1_times_w*im2).sum()/npix
     cov = p-s1*s2
     return cov,npix
-    
-    
+
 
 
 def compute_cov_direct(diff, w, parameters) :
@@ -131,6 +130,64 @@ def compute_cov_direct(diff, w, parameters) :
             if (dx==0 and dy == 0) : var = cov
             l.append((dx,dy,var,cov,npix))
     return l
+
+def cross_cov_direct_value(diff1, w1, diff2, w2, dx,dy):
+    """
+    Computes in direct space the covariance of image diff at lag (i,j)
+    for the pixels in w. Assumes that w contains 0's and 1's.
+    returns the covariance and the number of pixel pairs used to evaluate it.
+    """
+    (ncols,nrows) = diff1.shape
+    # switching both signs does not change anything:
+    # it just swaps im1 and im2 below
+    # not sure... for the cross correlation, do it anyway.
+    if (dx<0) : (dx,dy) = (-dx,-dy)
+    # now, we have dx >0. We have to distinguish two cases
+    # depending on dy sign
+    if dy>=0 :
+        im1 = diff1[dy:, dx:]
+        w1 = w1[dy:, dx:]
+        im2 = diff2[:ncols-dy, :nrows-dx]
+        w2=w2[:ncols-dy, :nrows-dx]
+    else:
+        im1 = diff1[:ncols+dy, dx:]
+        w1 = w1[:ncols+dy, dx:]
+        im2 = diff2[-dy:, :nrows-dx]
+        w2 = w2[-dy:, :nrows-dx]
+    # use the same mask for all 3 calculations
+    w_all = w1*w2
+    # do not use mean() because w=0 pixels would then count. 
+    npix = w_all.sum()
+    im1_times_w = im1*w_all
+    s1 = im1_times_w.sum()/npix
+    s2 = (im2*w_all).sum()/npix
+    p = (im1_times_w*im2).sum()/npix
+    cov = p-s1*s2
+    return cov,npix
+
+    
+def compute_cross_cov_direct(diff1, w1, diff2, w2, parameters) :
+    """
+    Compute covariances the old way. For more than ~25 lags
+    (parameters.maxrange), it is slower than the FFT way.
+    """
+    maxrange = parameters.maxrange
+    l=[]
+    var = 0
+    # (dy,dx) = (0,0) has to be first 
+    for dy in range(maxrange+1):
+        for dx in range (0,maxrange+1) :
+            if (dx*dy>0):
+                cov1,npix1 = cross_cov_direct_value(diff1, w1, diff2, w2, dx, dy)
+                cov2,npix2 = cross_cov_direct_value(diff1, w1, diff2, w2, dx,-dy)
+                cov = 0.5*(cov1+cov2)
+                npix = npix1+npix2
+            else:
+                cov,npix = cross_cov_direct_value(diff1, w1, diff2, w2,dx, dy)
+            if (dx==0 and dy == 0) : var = cov
+            l.append((dx,dy,var,cov,npix))
+    return l
+
 
 
 # assumes mask only contains 0's and 1's
@@ -240,6 +297,47 @@ class ComputeCov :
         tags = ['mu1','mu2','i','j','var','cov','npix','ext','s1','sp1','s2','sp2']+ self.other_tags
         return tuple_rows, tags
 
+
+    def compute_cross_cov(self):
+        if (im1.check_images(im2) == False) :
+            return [], []
+        tuple_rows = []
+        # store all data in order to read/prepare only once.
+        mus = {}
+        diffs = {}
+        ws = {}
+        for ext in self.extensions :
+            mu1,mu2, w, diff = self.diff_image(ext)
+            if mu1 is None : # something went wrong
+                continue
+            mus[ext] = 0.5*(mu1+mu2)
+            diffs[ext] = diff+ 0. # store a copy
+            ws[ext] = w + 0.
+        # double loop on extensions : 
+        for ext1,mu1 in mus.items():
+            diff1 = diffs[ext1]
+            w1 = ws[ext1]
+            #ped1, sig_ped1 = self.im1.ped_stat(ext)
+            #ped2, sig_ped2 = self.im2.ped_stat(ext)
+            channel1 = im1.channel_index(ext1)
+            # Not sure about the exact value that optimizes the whole thing
+            for ext2,mu2 in mus.items(): 
+                if ext2<=ext1 : continue
+                diff2 = diffs[ext2]
+                w2 = ws[ext2]
+                channel2 = im1.channel_index(ext2)
+                #if self.params.maxrange > 3 : 
+                #    sh = diff.shape
+                #    self.params.fft_shape = (fft_size(sh[0]+self.params.maxrange),
+                #                         fft_size(sh[1]+self.params.maxrange))
+                #    covs = compute_cov_fft(diff, w, self.params)
+                #else :
+                covs = compute_cross_cov_direct(diff1, w1, diff2, w2, params)
+                tuple_rows += [(mu1, mu2) + cov + (channel1, channel2) + self.other_values for cov in covs]
+        tags = ['mu1','mu2','i','j','var','cov','npix','ext1','ext2']+ self.other_tags
+        return tuple_rows, tags
+
+    
     
     
 import pickle
@@ -290,6 +388,11 @@ if __name__ == "__main__" :
                        dest = "correct_nonlinearity", 
                        help = "correct non linearity (using ./nonlin.pkl)")
 
+    parser.add_argument( "-c", "--cross-covariances", 
+                       action="store_true",  # default is False
+                       dest = "cross_cov", 
+                       help = "compute cross-amp covariances ")
+
     options = parser.parse_args()
     #    if (len(args) == 0) : 
     #        parser.print_help()
@@ -297,6 +400,11 @@ if __name__ == "__main__" :
 
     if options.maxrange is not None :
         params.maxrange= options.maxrange
+
+    if options.cross_cov and options.tuple_name == "tuple" :
+        print("renaming tuple to cross-tuple")
+        options.tuple_name = "cross-tuple"
+        
         
     try :
         file_handler = file_handlers[options.file_handler_tag]
@@ -330,7 +438,10 @@ if __name__ == "__main__" :
         cc = ComputeCov(im1,im2, params)
         t1 = im1.time_stamp()
         t2 = im2.time_stamp()
-        entries, tags = cc.compute_cov()
+        if options.cross_cov :
+            entries, tags = cc.compute_cross_cov()
+        else :
+            entries, tags = cc.compute_cov()
         if len(entries) == 0: continue
         # add t1, t2 at the end (tuples are immutable, hence new list) 
         tuple_entries = []
